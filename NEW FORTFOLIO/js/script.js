62978b3b706b20540ad32ce78aca8440d510b2f6
Sathish-1234 <99269433+Sathish-1234@users.noreply.github.com>
2023-02-02 08:54:12 +0530

    Add files via upload

---
+++ NEW FORTFOLIO/js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing",
+{
+    strings : ["Web Developer","UI/UX Designer"," Game Developer"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
+// <!-- for typing effect -->//
